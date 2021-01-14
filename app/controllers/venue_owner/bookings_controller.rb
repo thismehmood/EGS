3c56e99ca1ba9b3e5b41ae0a0090f4602d374e5d
thismehmood <this.mehmood@gmail.com>
2021-01-14 13:01:31 +0500

    final_viva_files

--- app/controllers/venue_owner/bookings_controller.rb
+++ app/controllers/venue_owner/bookings_controller.rb
@@ -11,7 +11,7 @@ class VenueOwner::BookingsController < ApplicationController
       @booking = Booking.find(params[:id])
         if @booking.destroy!
           BookingMailer.with(booking: @booking, venue:@venue).cancel_booking_email.deliver_now
-           redirect_to venue_owner_bookings_path, notice: 'Venue was successfully destroyed.'  
+           redirect_to venue_owner_bookings_path, notice: 'Booking was successfully destroyed.'  
         end
       end
 
